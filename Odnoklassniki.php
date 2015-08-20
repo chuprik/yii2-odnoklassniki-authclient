4996e5bfa8f467486fae54cf7743d416e9e44fcd
Constantine Chuprik <constantinchuprik@gmail.com>
2015-08-21 04:07:32 +0500

    Update Odnoklassniki.php

--- Odnoklassniki.php
+++ Odnoklassniki.php
@@ -45,10 +45,10 @@ class Odnoklassniki extends OAuth2
     protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
     {
         $params['access_token'] = $accessToken->getToken();
-        $params['application_key'] = $this->application_key;
+        $params['application_key'] = $this->applicationKey;
         $params['method'] = str_replace('/', '.', str_replace('api/', '', $url));
 
-        $first = 'application_key=' . $this->application_key . 'method=' . $params['method'];
+        $first = 'application_key=' . $this->applicationKey . 'method=' . $params['method'];
         $second = md5($params['access_token'] . $this->clientSecret);
 
         $params['sig'] = md5($first . $second);
